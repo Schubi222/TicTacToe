950b9b6ab03c5412516caa0aa32b132d16ae190f
Schubi222 <73842560+Schubi222@users.noreply.github.com>
2023-07-25 17:53:21 +0200

    some beginnings including buttons and part of homeview

--- TicTacToe/src/Views/HomeView.vue
+++ TicTacToe/src/Views/HomeView.vue
@@ -1,11 +1,37 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <div class="HomeView">
+    <div class="Top-Symbols">
+      <img src="" alt="">
+      <img src="" alt="">
+    </div>
+    <div class="SelectionWrapper">
+      <p class="SelectionWrapper__top-text">PICK PLAYER {{active_player}}'S MARK</p>
+      <div class="radio-wrapper">
+        <div class="radio_style">
+          <img src="/X.svg" alt="">
+          <input type="radio" id="X" name="mark" value="X">
+        </div>
+        <div class="radio_style">
+          <img src="/O.svg" alt="">
+          <input type="radio" id="O" name="mark" value="O">
+        </div>
+      </div>
+      <div class="SelectionWrapper__bottom-text">REMEMBER: X GOES FIRST</div>
+    </div>
+    <div class="yellow">NEW GAME (VS CPU)</div>
+    <div class="turquoise">NEW GAME (VS PLAYER)</div>
 
+  </div>
 </template>
 
-<style scoped>
+<script setup lang="ts">
 
+
+import {ref} from "vue";
+
+const active_player = ref(1)
+</script>
+
+<style scoped>
+  @import "@/assets/HomeView.css";
 </style>
